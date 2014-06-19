96b8079ac39c7662444ae717e4f8d3f152335f7f
Bram Wubs <bwubs@me.com>
2014-06-19 17:12:10 +0200

    Changes for backend page and config

--- app/code/community/Robinhq/Hooks/etc/api_config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Rename this file to api_config.php to properly work
-return array(
-    'apikey' => 'your-api-key',
-    'secret' => 'your-api-secret',
-    'baseUrl' => 'https://api.robinhq.com/'
-);
