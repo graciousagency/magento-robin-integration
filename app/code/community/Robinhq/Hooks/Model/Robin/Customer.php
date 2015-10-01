8d4b60a0e031008b68d9791f2d2fd01f13fb3904
Bram Wubs <bwubs@me.com>
2015-10-01 11:04:54 +0200

    added panel_view commend in customer model

--- app/code/community/Robinhq/Hooks/Model/Robin/Customer.php
+++ app/code/community/Robinhq/Hooks/Model/Robin/Customer.php
@@ -51,6 +51,7 @@ class Robinhq_Hooks_Model_Robin_Customer
             "customer_since" => Mage::getModel('core/date')->date('Y-m-d', strtotime($this->customer->getCreatedAt())),
             "order_count" => $orderCount,
             "total_spent" => $formattedTotalSpend,
+            //insert "panel_view" here for your additional data
             //optional data
             "name" => $this->customer->getName(),
             "currency" => Mage::app()->getStore()->getCurrentCurrencyCode(),
