f117ec1fb4d08a97a6f858adc12ae8931357d82b
Pepijn Blom <pepijn@graciousstudios.nl>
2015-12-15 12:12:20 +0100

    add libphonenumber

--- app/code/community/Robinhq/Hooks/Helper/Data.php
+++ app/code/community/Robinhq/Hooks/Helper/Data.php
@@ -1,10 +1,11 @@
 <?php
 
+
 /**
  * Class Robinhq_Hooks_Helper_Data
  */
-class Robinhq_Hooks_Helper_Data extends Mage_Core_Helper_Abstract
-{
+class Robinhq_Hooks_Helper_Data extends Mage_Core_Helper_Abstract {
+
     /**
      * @var Robinhq_Hooks_Model_Queue
      */
@@ -34,16 +35,14 @@ class Robinhq_Hooks_Helper_Data extends Mage_Core_Helper_Abstract
     /**
      * Gets and sets the dependency's
      */
-    public function __construct()
-    {
+    public function __construct() {
+
         $config = Mage::getStoreConfig('settings/general');
         $this->bulkLimit = (int)$config['bulk_limit'];
         $this->selectLimit = (int)$config['select_limit'];
-
         $this->logger = new Robinhq_Hooks_Model_Logger();
         $this->api = new Robinhq_Hooks_Model_Api($this->logger);
         $this->queue = new Robinhq_Hooks_Model_Queue($this->logger, $this->api, $this->bulkLimit);
-
         $this->converter = new Robinhq_Hooks_Model_Robin_Converter();
         $this->collector = new Robinhq_Hooks_Model_Collector($this->queue, $this->converter, $this->selectLimit);
     }
@@ -51,61 +50,234 @@ class Robinhq_Hooks_Helper_Data extends Mage_Core_Helper_Abstract
     /**
      * @param $message
      */
-    public function log($message)
-    {
+    public function log($message) {
+
         $this->logger->log($message);
     }
 
     /**
      * @return Robinhq_Hooks_Model_Queue
      */
-    public function getQueue()
-    {
+    public function getQueue() {
+
         return $this->queue;
     }
 
     /**
      * @return Robinhq_Hooks_Model_Api
      */
-    public function getApi()
-    {
+    public function getApi() {
+
         return $this->api;
     }
 
-    public function getLogger()
-    {
+    public function getLogger() {
+
         return $this->logger;
     }
 
-    public static function warnAdmin($warning)
-    {
+    public static function warnAdmin($warning) {
+
         Mage::getSingleton('adminhtml/session')->addWarning("Robin: " . $warning);
     }
 
-    public static function noticeAdmin($notice)
-    {
+    public static function noticeAdmin($notice) {
+
         Mage::getSingleton('adminhtml/session')->addSuccess("Robin: " . $notice);
     }
 
-    public static function formatPrice($price)
-    {
+    public static function formatPrice($price) {
+
         return Mage::helper('core')->currency($price, true, false);
     }
 
     /**
      * @return Robinhq_Hooks_Model_Robin_Converter
      */
-    public function getConverter()
-    {
+    public function getConverter() {
+
         return $this->converter;
     }
 
     /**
      * @return Robinhq_Hooks_Model_Collector
      */
-    public function getCollector()
-    {
+    public function getCollector() {
+
         return $this->collector;
     }
+
+    /**
+     * @param $phoneNumber
+     * @return mixed|string
+     */
+    public function formatPhoneNumber($phone) {
+
+        $kentallen = ['0909',
+            '0906',
+            '0900',
+            '0842',
+            '0800',
+            '0676',
+            '06',
+            '010',
+            '046',
+            '0111',
+            '0475',
+            '0113',
+            '0478',
+            '0114',
+            '0481',
+            '0115',
+            '0485',
+            '0117',
+            '0486',
+            '0118',
+            '0487',
+            '013',
+            '0488',
+            '015',
+            '0492',
+            '0161',
+            '0493',
+            '0162',
+            '0495',
+            '0164',
+            '0497',
+            '0165',
+            '0499',
+            '0166',
+            '050',
+            '0167',
+            '0511',
+            '0168',
+            '0512',
+            '0172',
+            '0513',
+            '0174',
+            '0514',
+            '0180',
+            '0515',
+            '0181',
+            '0516',
+            '0182',
+            '0517',
+            '0183',
+            '0518',
+            '0184',
+            '0519',
+            '0186',
+            '0521',
+            '0187',
+            '0522',
+            '020',
+            '0523',
+            '0222',
+            '0524',
+            '0223',
+            '0525',
+            '0224',
+            '0527',
+            '0226',
+            '0528',
+            '0227',
+            '0529',
+            '0228',
+            '053',
+            '0229',
+            '0541',
+            '023',
+            '0543',
+            '024',
+            '0544',
+            '0251',
+            '0545',
+            '0252',
+            '0546',
+            '0255',
+            '0547',
+            '026',
+            '0548',
+            '0294',
+            '055',
+            '0297',
+            '0561',
+            '0299',
+            '0562',
+            '030',
+            '0566',
+            '0313',
+            '0570',
+            '0314',
+            '0571',
+            '0315',
+            '0572',
+            '0316',
+            '0573',
+            '0317',
+            '0575',
+            '0318',
+            '0577',
+            '0320',
+            '0578',
+            '0321',
+            '058',
+            '033',
+            '0591',
+            '0341',
+            '0592',
+            '0342',
+            '0593',
+            '0343',
+            '0594',
+            '0344',
+            '0595',
+            '0345',
+            '0596',
+            '0346',
+            '0597',
+            '0347',
+            '0598',
+            '0348',
+            '0599',
+            '035',
+            '070',
+            '036',
+            '071',
+            '038',
+            '072',
+            '040',
+            '073',
+            '0411',
+            '074',
+            '0412',
+            '075',
+            '0413',
+            '076',
+            '0416',
+            '077',
+            '0418',
+            '078',
+            '043',
+            '079',
+            '045',
+        ];
+        $phone = preg_replace('/[^0-9]/', '', $phone);
+        if (substr($phone, 0, 2) == '00' && substr($phone, 2, 2) == '31') {
+            $phone = preg_replace('/^([0-9]{4})/', '0', $phone);
+        }
+        if (substr($phone, 0, 2) == '00') {
+            $phone = preg_replace('/^([0-9]{4})([0-9]+)/', '($1) $2', $phone);
+        }
+        for ($i = 4; $i >= 0; $i--) {
+            $ken = substr($phone, 0, $i);
+            if (in_array($ken, $kentallen)) {
+                break;
+            }
+        }
+        return preg_replace('/([0-9]{' . $i . '})([0-9]+)/', "$1-$2", $phone);
+    }
+
+
 }
 	 
--- app/code/community/Robinhq/Hooks/Model/Robin/Customer.php
+++ app/code/community/Robinhq/Hooks/Model/Robin/Customer.php
@@ -39,28 +39,46 @@ class Robinhq_Hooks_Model_Robin_Customer
     private function make()
     {
         $lifetime = $this->getLifeTimeSalesCustomer();
-
         $formattedTotalSpend = Mage::helper('core')->currency($lifetime->getLifetime(), true, false);
-
-        $twitterHandler = $this->getTwitterHandler();
 
         $phoneNumber = $this->getCustomerPhoneNumber();
         $orderCount = $lifetime->getNumOrders();
-        $this->data = array(
-            "email_address" => $this->customer->getEmail(),
-            "customer_since" => Mage::getModel('core/date')->date('Y-m-d', strtotime($this->customer->getCreatedAt())),
-            "order_count" => $orderCount,
-            "total_spent" => $formattedTotalSpend,
-            //optional data
-            "name" => $this->customer->getName(),
-            "currency" => Mage::app()->getStore()->getCurrentCurrencyCode(),
-            "phone_number" => $phoneNumber,
-            "twitter_name" => $twitterHandler
+        $rewardPoints = $this->getRewardPoints();
 
-        );
-
+        $this->data = [
+            'email_address'     => $this->customer->getEmail(),
+            'customer_since'    => Mage::getModel('core/date')->date('Y-m-d', strtotime($this->customer->getCreatedAt())),
+            'order_count'       => $orderCount,
+            'total_spent'       => $formattedTotalSpend,
+            "panel_view" => [
+                'Reward_points' => $rewardPoints,
+            ],
+            'name'              => $this->customer->getName(),
+            'currency'          => Mage::app()->getStore()->getCurrentCurrencyCode(),
+            'phone_number'      => $phoneNumber,
+            'reward_points'     => $rewardPoints,
+        ];
         return $this->data;
     }
+
+    private function getRewardPoints() {
+        $allStores = Mage::app()->getStores();
+        $points = 0;
+        if($this->customer->getId())    {
+            foreach ($allStores as $_eachStoreId => $val) {
+                $_storeId = Mage::app()->getStore($_eachStoreId)->getId();
+                if (Mage::getStoreConfig('rewardpoints/default/flatstats', $_storeId)) {
+                    $reward_flat_model = Mage::getModel('rewardpoints/flatstats');
+                    $points += $reward_flat_model->collectPointsCurrent($this->customer->getId(), $_storeId) + 0;
+                } else {
+                    $reward_model = Mage::getModel('rewardpoints/stats');
+                    $points += $reward_model->getPointsCurrent($this->customer->getId(), $_storeId) + 0;
+                }
+            }
+        }
+        return $points;
+    }
+
 
     /**
      * Gets customer statics like total order count and total spend.
@@ -107,7 +125,8 @@ class Robinhq_Hooks_Model_Robin_Customer
     private function getTwitterHandler()
     {
         return ($this->customer->getTwitterHandler() === null) ? "" : $this->customer->getTwitterHandler();
-
     }
+
+
 
 }
