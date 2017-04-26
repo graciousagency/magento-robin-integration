359c19e5886136269bc44ad79db68cf6acbe8608
Jeroen <jeroen@srcode.nl>
2017-04-26 15:43:19 +0200

    Refactored API to allow rewrites on models

--- app/code/community/Robinhq/Hooks/Model/Api.php
+++ app/code/community/Robinhq/Hooks/Model/Api.php
@@ -5,29 +5,28 @@
  * A small wrapper to communicate with the Robin API.
  * Class Robinhq_Hooks_Model_Api
  */
-class Robinhq_Hooks_Model_Api {
+class Robinhq_Hooks_Model_Api
+{
 
-    /**
-     * @var Robinhq_Hooks_Model_Logger
-     */
-    private $logger;
+    /** @var Robinhq_Hooks_Helper_Data */
+    protected $helper;
 
     /**
      * Gets and sets the dependency's
-     * @param Robinhq_Hooks_Model_Logger $logger
+     * @param Robinhq_Hooks_Helper_Data $helper
      */
-    public function __construct(Robinhq_Hooks_Model_Logger $logger) {
-
-        $this->logger = $logger;
+    public function __construct(Robinhq_Hooks_Helper_Data $helper)
+    {
+        $this->helper = $helper;
     }
 
-    public function customer($customer) {
-
+    public function customer($customer)
+    {
         return $this->customers([$customer]);
     }
 
-    public function order($order) {
-
+    public function order($order)
+    {
         return $this->orders([$order]);
     }
 
@@ -38,9 +37,9 @@ class Robinhq_Hooks_Model_Api {
      * @param $customers
      * @return mixed
      */
-    public function customers($customers) {
-
-        $this->logger->log('Sending customers to ROBIN');
+    public function customers($customers)
+    {
+        $this->helper->log('Sending customers to ROBIN');
         return $this->post('customers', $customers);
     }
 
@@ -51,9 +50,9 @@ class Robinhq_Hooks_Model_Api {
      * @param $orders
      * @return mixed
      */
-    public function orders($orders) {
-
-        $this->logger->log('Sending orders to ROBIN');
+    public function orders($orders)
+    {
+        $this->helper->log('Sending orders to ROBIN');
         return $this->post('orders', $orders);
     }
 
@@ -65,19 +64,28 @@ class Robinhq_Hooks_Model_Api {
      * @throws Exception
      * @return resource
      */
-    private function setUpCurl($request) {
+    protected function setUpCurl($request)
+    {
+        $helper = $this->helper;
 
-        $config = Mage::getStoreConfig('settings/general');
-        if (!empty($config['api_key']) && !empty($config['api_secret'])) {
-            $url = $config['api_url'] . '/' . $request;
-            $this->logger->log("Posting to [" . $url . "]");
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_USERPWD, $config['api_key'] . ":" . $config['api_secret']);
-            return $ch;
+        $apiUrl = $helper->getConfig('api_url');
+        $apiKey = $helper->getConfig('api_key');
+        $apiSecret = $helper->getConfig('api_secret');
+
+        if (empty($apiKey) || empty($apiSecret)) {
+            throw new Exception('Missing API configuration, go to System -> Configuration -> ROBINHQ -> Settings and fill in your API credentials');
         }
-        throw new Exception('Missing API configuration, go to System -> Configuration -> ROBINHQ -> Settings and fill in your API credentials');
+
+        $url = $apiUrl . '/' . $request;
+
+        $this->helper->log("Posting to [" . $url . "]");
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERPWD, $apiKey . ":" . $apiSecret);
+
+        return $ch;
     }
 
     /**
@@ -93,17 +101,26 @@ class Robinhq_Hooks_Model_Api {
      * @throws Robinhq_Hooks_Model_Exception_UnauthorizedException
      * @throws Robinhq_Hooks_Model_Exception_UnknownStatusCodeException
      */
-    private function post($request, $values) {
-
+    protected function post($request, $values)
+    {
         $errorCodes = $this->getErrorCodes();
         $ch = $this->prepare($request, $values);
+
         $responseInfo = $this->execute($ch);
-        $this->logger->log("Robin returned status: " . $responseInfo['http_code']);
+
+        $this->helper->log("Robin returned status: " . $responseInfo['http_code']);
         if (in_array($responseInfo['http_code'], $errorCodes)) {
-            $error = Robinhq_Hooks_Model_Exception_RequestFailed::factory($responseInfo['http_code']);
-            $this->logger->log($error->getMessage());
+
+            /** @var Robinhq_Hooks_Model_Exception_RequestFailed $requestFailedFactory */
+            $requestFailedFactory = Mage::getModel('robinhq_exception_RequestFailed');
+
+            $error = $requestFailedFactory::factory($responseInfo['http_code']);
+
+            $this->helper->log($error->getMessage());
+
             throw $error;
         }
+
         return $responseInfo;
     }
 
@@ -112,14 +129,21 @@ class Robinhq_Hooks_Model_Api {
      * @return mixed
      * @throws Robinhq_Hooks_Model_Exception_RequestImpossibleException
      */
-    private function execute($ch) {
-
-        if (curl_exec($ch) === false) {
+    protected function execute($ch)
+    {
+        if (false === curl_exec($ch)) {
             curl_close($ch);
-            throw new Robinhq_Hooks_Model_Exception_RequestImpossibleException("Error: 'Unable to preform request to Robin, request was not executed.'");
+
+            $error = Mage::getModel('robinhq_hooks/exception_requestImpossibleException', [
+                    "Error: 'Unable to preform request to Robin, request was not executed.'"
+            ]);
+
+            throw new $error;
         }
+
         $responseInfo = curl_getinfo($ch);
         curl_close($ch);
+
         return $responseInfo;
     }
 
@@ -129,47 +153,54 @@ class Robinhq_Hooks_Model_Api {
      * @return resource
      * @throws Exception
      */
-    private function prepare($request, $values) {
+    protected function prepare($request, $values)
+    {
 
         // Start extra check to make sure we are not passing any customers or orders without email
-        $aKeysToRemove = [];
-        foreach($values as $key=>$value)    {
-            if(empty($value['email_address']))  {
-                $aKeysToRemove[] = $key;
-            }
-        }
-        foreach($values as $key=>$value)    {
-            if(!in_array($key, $aKeysToRemove)) {
-                $valuesNew[] = $value;
+        foreach ($values as $key => $value) {
+            if (empty($value['email_address'])) {
+                unset($values[$key]);
             }
         }
         // End extra email check
-        $values = json_encode([$request => $valuesNew]);
-        $ch = $this->setUpCurl($request);
+
+        $values = json_encode([$request => $values]);
         $valuesLength = strlen($values);
-        $this->logger->log("Posting with: " . $values);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json',
+
+        $ch = $this->setUpCurl($request);
+
+        $this->helper->log("Posting with: " . $values);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json',
                 'Content-Length: ' . $valuesLength,
-            ]);
+        ]);
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, $values);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//        // Debug request
 //        curl_setopt($ch, CURLOPT_VERBOSE, true);
 //        $fp = fopen(dirname(__FILE__) . '/robin_curl_output.txt', 'w');
 //        curl_setopt($ch, CURLOPT_STDERR, $fp);
+
         return $ch;
     }
 
     /**
      * @return array
      */
-    private function getErrorCodes() {
-
-        $codes = new Robinhq_Hooks_Model_Robin_StatusCode();
-        $errorCodes = [$codes::INTERNAL_SERVER_ERROR,
-            $codes::BAD_REQUEST,
-            $codes::UNAUTHORIZED,
-            $codes::RATE_LIMIT_EXCEEDED,
+    protected function getErrorCodes()
+    {
+        /** @var Robinhq_Hooks_Model_Robin_StatusCode $codes */
+        $codes = Mage::getModel('robinhq_hooks/robin_statusCode');
+        $errorCodes = [
+                $codes::INTERNAL_SERVER_ERROR,
+                $codes::BAD_REQUEST,
+                $codes::UNAUTHORIZED,
+                $codes::RATE_LIMIT_EXCEEDED,
         ];
+
         return $errorCodes;
     }
 
